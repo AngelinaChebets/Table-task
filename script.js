87e2374df5bad7c8899e0eb1bc27e9dde0982527
AngelinaChebets <84124516+AngelinaChebets@users.noreply.github.com>
2021-05-30 16:35:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+"use strict";
+
+function findParentByTagName (elem, tagName) {
+  tagName = tagName.toLowerCase();
+  while (elem && elem.parentNode) {
+    elem = elem.parentNode;
+    if (elem.tagName && elem.tagName.toLowerCase() == tagName) {
+      return elem;
+    }
+  }
+ return null;
+}
+
+function deleteRowFromTable (target) { 
+  let parent = findParentByTagName(target, 'tr');  
+  parent.remove(); 
+}
+
+tableOfFilms.onclick = function(event){
+  let target = event.target.closest('.deleteButton');
+  if (target != null) {
+    deleteRowFromTable(target);
+    return;
+  }  
+}
+    
   
